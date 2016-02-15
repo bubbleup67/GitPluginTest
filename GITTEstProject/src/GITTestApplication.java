93515d31d13023d2cde2ddde4bf3e73dc5e4990d
bubbleup <bubbleup@10.0.1.9>
2016-02-15 14:56:23 -0500

    First test GIT Commit - JohnRay

---
+++ GITTEstProject/src/GITTestApplication.java
@@ -0,0 +1,9 @@
+
+public class GITTestApplication {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld This is Fun");
+		System.out.println("HelloWorld This is Fun Again");
+	}
+}
